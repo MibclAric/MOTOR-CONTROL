d3ab915ae349b7803d2663f34ee0de897e4a41dd
MibclAric <32399727+MibclAric@users.noreply.github.com>
2019-11-11 08:22:37 -0500

    Update Phase B.ino

--- Phase B.ino
+++ Phase B.ino
@@ -3,158 +3,105 @@
 #include <CytronMotorDriver.h>
 
 double sensorDist;
-
 CytronMD motor1(PWM_DIR, 5, 4);  // PWM 1 = Pin 5, DIR 1 = Pin 4.
 CytronMD motor2(PWM_DIR, 6, 7); // PWM 2 = Pin 6, DIR 2 = Pin 7.
 
-Encoder WRight(24,25);
-Encoder WLeft(23,22);
-double Ldist = 0;
-double Rdist = 0;
+Encoder WRight(24,25); Encoder WLeft(23,22);
+double Ldist = 0;double Rdist = 0;
 
-double LSetSpeed ; double LOutput;
-double RSetSpeed ; double ROutput;
+double LSetSpeed ; double LOutput; double RSetSpeed ; double ROutput;
 
-double OldLeftSpeed = 0;
-double LeftDerivative  = 0;
-double LeftErrorSum = 0;
-
-double OldRightSpeed = 0;
-double RightDerivative = 0;
-double RightErrorSum = 0;
+double OldLeftSpeed = 0;double LeftDerivative  = 0;double LeftErrorSum = 0;
+double OldRightSpeed = 0;double RightDerivative = 0;double RightErrorSum = 0;
 
 double LSpeed = 0; double RSpeed = 0;
-double Kp=10;double Ki=5 ;double Kd=0;
-double Kp2=10;double Ki2=5;double Kd2=0;
+double Kp=10;double Ki=5 ;double Kd=0;double Kp2=10;double Ki2=5;double Kd2=0;
 
-   
-  double Lerror = 0; 
-  double Lcontrol = 0;
-  double LFeedForward = 0;
-
-  double Rerror = 0;
-  double Rcontrol = 0;
-  double RFeedForward = 0;
+double Lerror = 0;  double Lcontrol = 0;double LFeedForward = 0;
+double Rerror = 0;double Rcontrol = 0;double RFeedForward = 0;
 
 
 //*****************************************************************************//
 void setup() {
   Serial.begin(9600);
-  Serial1.begin(9600); 
-
+  Serial1.begin(9600);
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-
+  char pose1='a'; char pose2='b'; char pose3 = 'c';
   Timer1.attachInterrupt(TimerInterrupt);
   Timer1.start(100000); //every 0.1 seconds
-  
-  delay(1000);
-    long currentDist = 0;
-    int Task1 = 0;
+  //********************START*********************??
 
+  delay(2000);
 
-while(Task1<38){
-  LSetSpeed = 15;
-  RSetSpeed = 15;
-  speedCorrection();
-  Task1 = ticksToCM(WLeft.read());
-  delay(50);
-}
-frontCheck(690);
+task(42,13);
 setStop();
-delay(500);
+delay(200);
 rightTurn();
-delay(500);
 
-int Task2 = 0;
-    startSpeed(15);
- currentDist = ticksToCM(WLeft.read());
-while(Task2-currentDist<=25){
-  LSetSpeed = 15;
-  RSetSpeed = 15;
-  speedCorrection();
-  Task2 = ticksToCM(WLeft.read());
-  delay(50);
-}
-frontCheck(690);
+task(42,13);
 setStop();
-delay(500);
+delay(200);
 leftTurn();
-delay(500);
 
-int Task3 = 0;
-    startSpeed(15);
- currentDist = ticksToCM(WLeft.read());
-while(Task3-currentDist<=50){
-  LSetSpeed = 15;
-  RSetSpeed = 15;
-  speedCorrection();
-  Task3 = ticksToCM(WLeft.read());
-  delay(50);
-}
-frontCheck(690);
+
+task(62,13);
 setStop();
-delay(500);
+delay(200);
 leftTurn();
-delay(500);
 
 
-int Task4 = 0;
-    startSpeed(15);
- currentDist = ticksToCM(WLeft.read());
-while(Task4-currentDist<=35){
-  LSetSpeed = 15;
-  RSetSpeed = 15;
-  speedCorrection();
-  Task4 = ticksToCM(WLeft.read());
-  delay(50);
-}
-frontCheck(463);
+task(36,13);
 setStop();
-delay(500);
 rightTurn();
 delay(500);
-
-frontCheck(690);
-setStop();
-delay(500);
+frontCheck(730);
 rightTurn();
-delay(500);
+
+  Serial1.write(pose1);
+  delay(100);
 //*******************Lower Level************************//
-int Task5 = 0;
-    startSpeed(15);
- currentDist = ticksToCM(WLeft.read());
-while(Task5-currentDist<=50){
-  LSetSpeed = 15;
-  RSetSpeed = 15;
-  speedCorrection();
-  Task5 = ticksToCM(WLeft.read());
-  delay(50);
-}
-frontCheck(685);
-setStop();
-delay(500);
+task(50,6);
+frontCheck(700);
 rightTurn();
+ Serial1.write(pose2);
+ delay(200);
+task(130,10);
+frontCheck(700);
+rightTurn();
+
+
+task(63,13);
+setStop();
+rightTurn();
+
+task(5,13);
+frontCheck(730);
+rightTurn();
+
+task(30,13);
+frontCheck(700);
+leftTurn();
+
+task(82,13);
+setStop();
+leftTurn();
+
+task(40,13);
+setStop();
+leftTurn();
+Serial1.write(pose3);
 delay(500);
 
 }
 //*****************************************************************************//
-void loop() 
-{
-
-
-}
-
+void loop() {
+ 
+  }
 void TimerInterrupt()
 {
   measureSpeeds();
   controlSpeeds();
-  
-  Serial.print("speedL: ");
-  Serial.println(LSpeed);
-  Serial.print("speedR: ");
-  Serial.println(RSpeed);
-
 }
 
 //*****************************************************************************//
@@ -235,43 +182,46 @@ void sensorRead(){
 void speedCorrection(){
   sensorRead();
   if(sensorDist<13){
-    LSetSpeed = LSetSpeed - 1.25;
-        Serial1.println("Left - - ");
- 
+    LSetSpeed = LSetSpeed - 1.2;
+    Serial.println(sensorDist);
   }
   else if(sensorDist>13&&sensorDist<18){
-    RSetSpeed = RSetSpeed - 1.25;
-       Serial1.println("Right - - ");
-  }
-  Serial1.println(sensorDist);
-   
+    RSetSpeed = RSetSpeed - 1.2;
+    Serial.println(sensorDist);
+  } 
 }
 
 //*****************************************************************************//
 void leftTurn(){
     float currentRRead =ticksToCM(WRight.read());
-    setStop();
-  while(ticksToCM(WRight.read())-currentRRead < 9){
-    RSetSpeed = 10;
-    LSetSpeed = -10;
-  }
-  setStop();
-}
-void rightTurn(){
     float currentLRead =ticksToCM( WLeft.read());
     setStop();
-  while(ticksToCM(WLeft.read())-currentLRead <9){
-    RSetSpeed = -10;
-    LSetSpeed = 10;
+  while(((ticksToCM(WRight.read())-currentRRead)+(currentLRead-ticksToCM(WLeft.read())))/2 < 9){
+    RSetSpeed = 8;
+    LSetSpeed = -8;
   }
   setStop();
+  delay(300);
 }
-void setStop(){
 
+void rightTurn(){
+    float currentLRead =ticksToCM( WLeft.read());
+    float currentRRead =ticksToCM(WRight.read());
+    setStop();
+  while(((ticksToCM(WLeft.read())-currentLRead)+(currentRRead-ticksToCM(WRight.read())))/2 <9){
+    RSetSpeed = -8;
+    LSetSpeed = 8;
+  }
+  setStop();
+  delay(300);
+}
+
+void setStop(){
   RSetSpeed = 0;
   LSetSpeed = 0;
-  
+//  Serial1.println("Stopped");
 }
+
 void startSpeed(int speedset){
   for(int d=0;d<=speedset;d++){
       RSetSpeed = d;
@@ -280,9 +230,25 @@ void startSpeed(int speedset){
   }
 }
 void frontCheck(int frontset){
-  while(analogRead(A1)<frontset){
-    RSetSpeed = LSetSpeed = 6+9*((frontset-analogRead(A1))/(frontset));
-    Serial1.println(analogRead(A1));
+  while(analogRead(A1)<=frontset){
+    RSetSpeed = LSetSpeed = 6+5*((frontset-analogRead(A1))/(frontset));
+  // Serial1.println(analogRead(A1));
   }
   setStop();
+  delay(300);
+}
+void task(int taskdist,int taskspeed){
+  long currentDist = ticksToCM(WLeft.read());
+  int Task = 0;
+  startSpeed(taskspeed);
+while(Task-currentDist<=taskdist){
+    LSetSpeed = taskspeed;
+    RSetSpeed = taskspeed;
+    speedCorrection();
+    Task = ticksToCM(WLeft.read());
+    delay(80);
+//    Serial1.print("Distance is : ");
+//    Serial1.println(Task);
+  }
+  delay(300);
 }
